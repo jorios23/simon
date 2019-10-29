444f13f3e35c11487faf9011f041e6fe4b248157
jorios23 <54322660+jorios23@users.noreply.github.com>
2019-10-29 10:05:02 -0600

    Add files via upload

---
+++ simon.py
@@ -0,0 +1,13 @@
+import time
+arreglo = [A, B, C, D, E]
+import random 
+randint.(arreglo)
+print ("Bienvenido, vamos a jugar Simon")
+saludo = input("Cual es tu nombre?")
+print ("This is printed immmediately.")
+time.sleep(1)
+print ("3")
+time.sleep(1)
+print ("2")
+time.sleep(1)
+print ("1")
